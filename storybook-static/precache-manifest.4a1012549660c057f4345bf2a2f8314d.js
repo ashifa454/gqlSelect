96544eaaed11a32fe9555c85bd00a973cdc6dba5
asif ansari <ashifa454@gmail.com>
2019-09-13 00:38:05 +0530

    Fixes

---
+++ storybook-static/precache-manifest.4a1012549660c057f4345bf2a2f8314d.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64c3515d4e7cea0837b4d157ed2ff15",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5c66f5d730326111c357.bundle.js"
+  },
+  {
+    "url": "runtime~main.5c66f5d730326111c357.bundle.js"
+  },
+  {
+    "url": "vendors~main.5c66f5d730326111c357.bundle.js"
+  }
+]);
